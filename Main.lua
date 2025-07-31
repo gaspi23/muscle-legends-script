c90e50d5acd4738a8f7d7817591ccd0b9bf6ecba
gaspi23 <luisgcruzhdez@gmail.com>
2025-07-31 15:19:01 -0600

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,17 +1,11 @@
-local mt = getrawmetatable(game)
-local namecall = mt.__namecall
-local index = mt.__index
-setreadonly(mt, false)
-
-mt.__namecall = newcclosure(function(self, ...)
-    local method = getnamecallmethod()
-    if method == "FireServer" then
-        print("[FireServer detected] →", self:GetFullName(), ...)
+for _,v in pairs(game:GetService("ReplicatedStorage"):GetDescendants()) do
+    if v:IsA("RemoteEvent") then
+        v.OnClientEvent:Connect(function(...)
+            print("📡 RemoteEvent:", v.Name, ...)
+        end)
+    elseif v:IsA("RemoteFunction") then
+        v.OnClientInvoke = function(...)
+            print("📡 RemoteFunction:", v.Name, ...)
+        end
     end
-    return namecall(self, ...)
-end)
-
-mt.__index = newcclosure(function(self, key)
-    return index(self, key) -- avoids saturation
-end)
-
+end
